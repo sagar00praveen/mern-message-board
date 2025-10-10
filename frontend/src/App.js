f91fb19cf55b1609e0a678d40c3ba6d269f5fe22
sagar praveen <168666276+sagar00praveen@users.noreply.github.com>
2025-10-11 01:58:05 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = 'http://localhost:5000/api/messages';
+const API_URL = 'https://mern-message-board-nzbs.onrender.com/api/messages';
+
 
 function App() {
   const [messages, setMessages] = useState([]);
